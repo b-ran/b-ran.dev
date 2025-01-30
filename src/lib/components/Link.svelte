b43412ad7ab750962895158ce28e326d5cb01528
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-01-30 23:15:39 +1300

    Add link comp

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let {
+		href,
+		target = '_self',
+		children
+	} = $props();
+
+</script>
+
+<a {href} {target} class="flex items-center hover:underline hover:underline-offset-4">
+	{@render children()}
+</a>
