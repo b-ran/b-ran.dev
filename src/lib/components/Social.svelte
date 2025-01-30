52bd2fe3178a8d368d372a937f3726e632a5ede3
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-01-30 23:15:01 +1300

    Add social links

---
+++ src/lib/components/Social.svelte
@@ -0,0 +1,40 @@
+<script>
+	// Components
+	import ListButton from '$lib/components/buttons/ListButton.svelte';
+	import Email from '$lib/components/icons/Email.svelte';
+	import GitHub from '$lib/components/icons/GitHub.svelte';
+	import LinkedIn from '$lib/components/icons/LinkedIn.svelte';
+	import Instagram from '$lib/components/icons/Instagram.svelte';
+	import Link from '$lib/components/Link.svelte';
+</script>
+
+<ul class="flex flex-row gap-4 items-center list-none">
+	<li>
+		<ListButton>
+			<Link href="mailto:bran.scotthill@gmail.com" target="_blank">
+				<Email />
+			</Link>
+		</ListButton>
+	</li>
+	<li>
+		<ListButton>
+			<Link href="https://github.com/b-ran" target="_blank">
+				<GitHub />
+			</Link>
+		</ListButton>
+	</li>
+	<li>
+		<ListButton>
+			<Link href="https://www.linkedin.com/in/b-ran" target="_blank">
+				<LinkedIn />
+			</Link>
+		</ListButton>
+	</li>
+	<li>
+		<ListButton>
+			<Link href="https://www.instagram.com/scotthillbrandon" target="_blank">
+				<Instagram />
+			</Link>
+		</ListButton>
+	</li>
+</ul>
