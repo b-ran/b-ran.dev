fa7adba61b152ae0c3b1e599fe172a09d995ecfe
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-01-30 23:15:56 +1300

    Add root page with contact form

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import TopButton from '$lib/components/buttons/TopButton.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="mt-10 max-w-[1200px] px-10 mx-auto text-zinc-900 dark:text-blue-50 ">
+	<Header />
+	{@render children()}
+	<Footer />
+</div>
+
+<TopButton />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+	// Svelte
+	import { enhance } from '$app/forms';
+
+	// Components
+	import Social from '$lib/components/Social.svelte';
+	import Link from '$lib/components/Link.svelte';
+	import RightArrow from '$lib/components/icons/RightArrow.svelte';
+	import Button from '$lib/components/buttons/Button.svelte';
+	import Project from '$lib/components/Project.svelte';
+
+	let { form } = $props();
+
+	let projects = [
+		{
+			title: 'Project 1',
+			description: 'This is a description of project 1',
+			image: 'https://placehold.co/850',
+			href: 'https://google.com'
+		},
+		{
+			title: 'Project 2',
+			description: 'This is a description of project 2',
+			image: 'https://placehold.co/850',
+			href: 'https://google.com'
+		},
+		{
+			title: 'Project 3',
+			description: 'This is a description of project 3',
+			image: 'https://placehold.co/850',
+			href: 'https://google.com'
+		},
+		{
+			title: 'Project 4',
+			description: 'This is a description of project 4',
+			image: 'https://placehold.co/850',
+			href: 'https://google.com'
+		},
+	]
+
+</script>
+
+<title>Brandon Scott-Hill</title>
+
+<main>
+	<section class="pt-48 grid md:grid-cols-2 grid-cols-1 gap-20">
+
+		<div>
+			<h1 class="font-extrabold text-5xl leading-snug mb-5">
+				Hello there, I'm Brandon Scott-Hill
+			</h1>
+
+			<p class="text-xl mb-5">
+				A <b>Software Engineer</b> who uses creativity and experience to solve problems.
+				Creates clear, maintainable code and works closely with different teams
+				to deliver results.
+			</p>
+
+			<Social />
+		</div>
+
+		<div>
+			<div class="text-2xl bg-cyan-300 h-96 rounded-3xl"></div>
+		</div>
+	</section>
+
+	<section class="mt-32">
+		<div class="flex flex-col md:flex-row md:items-center md:justify-between gap-2">
+			<h1 class="font-extrabold text-4xl">Highlighted Projects</h1>
+			<Link href="projects">
+				<span class="font-semibold mr-2">View all </span>
+				<RightArrow />
+			</Link>
+		</div>
+
+		<div class="mt-12 grid grid-cols-1 md:grid-cols-2 gap-4">
+			{#each projects as project}
+				<Project {...project} />
+			{/each}
+<!--			<div class="text-2xl bg-cyan-300 h-96 rounded-3xl"></div>-->
+<!--			<div class="text-2xl bg-cyan-300 h-96 rounded-3xl"></div>-->
+<!--			<div class="text-2xl bg-cyan-300 h-96 rounded-3xl"></div>-->
+<!--			<div class="text-2xl bg-cyan-300 h-96 rounded-3xl"></div>-->
+		</div>
+	</section>
+
+
+	<section class="max-w-2xl mx-auto mt-32">
+
+		<h1 id="contact" class="font-extrabold text-4xl mb-8 text-center">Send me a message!</h1>
+
+		<form method="POST" class="space-y-6" use:enhance>
+			<input
+				data-1p-ignore
+				type="text"
+				name="name"
+				placeholder="Your name"
+				required
+				class="mt-1 block w-full h-16 px-5 py-2 rounded-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 text-zinc-900"
+			/>
+
+			<input
+				data-1p-ignore
+				type="email"
+				name="email"
+				placeholder="Your email"
+				required
+				class="mt-1 block w-full h-16 px-5 py-2 rounded-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 text-zinc-900"
+			/>
+
+			<textarea
+				name="message"
+				placeholder="Your message"
+				required
+				class="mt-1 block w-full px-5 py-5 min-h-40 rounded-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 text-zinc-900"
+			></textarea>
+
+
+			<Button type="submit">Submit</Button>
+		</form>
+
+		{#if form && form.message}
+			<p class="mt-4 text-center">{form.message}</p>
+		{/if}
+
+	</section>
+</main>
