e9442cc889eb341ed47bb70d985df88265f7f241
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-02-07 17:58:22 +1300

    Populate project content

--- src/lib/shared/projects.ts
+++ src/lib/shared/projects.ts
@@ -1,52 +1,50 @@
+import theSauce from '$lib/assets/projects/the-sauce.png';
+import cc from '$lib/assets/projects/cc.png';
+import cvGen from '$lib/assets/projects/cv-gen.png';
+import dotPortal from '$lib/assets/projects/dot-portal.png';
+import nix from '$lib/assets/projects/nix.png';
+
 const projects = [
 	{
-		title: 'Project 1',
-		description: ' Created a new version of my portfolio website with a focus on clean design, improved user experience, and eye-catching UI animations.',
-		image: 'https://placehold.co/850',
-		href: 'https://google.com',
-		tags: ['Python', 'Django', 'FastAPI', 'Docker', 'PostgreSQL', 'Kubernetes', 'Professional'],
+		title: 'The Sauce',
+		description: 'A business insights and visualization web platform for ANZ Business Customers. Built in collaboration between DOT Loves Data and ANZ ',
+		image: theSauce,
+		href: 'https://sauce.anz.co.nz',
+		tags: ['SvelteKit', 'Typescript', 'TailwindCSS', 'Supabase', 'AWS', 'Docker'],
 		highlight: true
 	},
 	{
-		title: 'Project 2',
-		description: ' Created a new version of my portfolio website with a focus on clean design, improved user experience, and eye-catching UI animations.',
-		image: 'https://placehold.co/850',
-		href: 'https://google.com',
-		tags: ['Svelte', 'SvelteKit', 'TailwindCSS', 'AWS', 'Professional'],
+		title: 'Community Compass',
+		description: 'Web application that collates and visualizes data from across New Zealand to help community organizations make data-driven decisions.',
+		image: cc,
+		href: 'https://app.communitycompass.co.nz',
+		tags: ['SvelteKit', 'Typescript', 'TailwindCSS', 'AWS', 'Kubernetes', 'Docker'],
 		highlight: true
 	},
 	{
-		title: 'Project 3',
-		description: ' Created a new version of my portfolio website with a focus on clean design, improved user experience, and eye-catching UI animations.',
-		image: 'https://placehold.co/850',
-		href: 'https://google.com',
-		tags: ['React', 'Next.js', 'ChakraUI', 'Firebase', 'Professional'],
+		title: 'DOT Portal',
+		description: 'Django application that serves as a central hub for all DOT Loves Data products and services.',
+		image: dotPortal,
+		href: 'https://products.dotlovesdata.com',
+		tags: ['Django', 'Python', 'FastAPI', 'AWS', 'Kubernetes', 'Docker'],
 		highlight: true
 	},
 	{
-		title: 'Project 4',
-		description: ' Created a new version of my portfolio website with a focus on clean design, improved user experience, and eye-catching UI animations.',
-		image: 'https://placehold.co/850',
-		href: 'https://google.com',
-		tags: ['Java', 'Personal'],
+		title: 'CV PDF Generator',
+		description: 'A simple NodeJS application that generates my CV in PDF format.',
+		image: cvGen,
+		href: 'https://github.com/b-ran/cv-gen',
+		tags: ['Node' , 'Typescript', 'Javascript', 'PDFMake'],
 		highlight: true
 	},
 	{
-		title: 'Project 5',
-		description: ' Created a new version of my portfolio website with a focus on clean design, improved user experience, and eye-catching UI animations.',
-		image: 'https://placehold.co/850',
-		href: 'https://google.com',
-		tags: ['React Native', 'Expo', 'Firebase', 'Personal'],
+		title: 'NixOS Desktop',
+		description: 'My personal linux desktop configuration managed with NixOS and Home-Manager.',
+		image: nix,
+		href: 'https://github.com/b-ran/dots',
+		tags: ['Nix', 'NixOS', 'Home-Manager'],
 		highlight: false
 	},
-	{
-		title: 'Project 6',
-		description: ' Created a new version of my portfolio website with a focus on clean design, improved user experience, and eye-catching UI animations.',
-		image: 'https://placehold.co/850',
-		href: 'https://google.com',
-		tags: ['Python' , 'Django', 'PostgreSQL', 'Professional'],
-		highlight: false
-	}
 ];
 
 export default projects;
