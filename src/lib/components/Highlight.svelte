652d51e639eac09cead360fa9f856a3a5f2be9f2
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-02-08 21:50:15 +1300

    Add dark text to feature text

--- src/lib/components/Highlight.svelte
+++ src/lib/components/Highlight.svelte
@@ -18,7 +18,7 @@
 <div class="flex flex-col items-center {alternate ? 'lg:flex-row' : 'lg:flex-row-reverse'} gap-5 lg:gap-10">
 	<div class="flex flex-col gap-4 text-left lg:w-1/2">
 		<h2 class="text-2xl">{title}</h2>
-		<p class="text-neutral-600">{description}</p>
+		<p class="text-neutral-600 dark:text-blue-50">{description}</p>
 
 		<div class="flex flex-wrap gap-3 mb-3">
 			{#each tags.slice(0, 10) as tag}
