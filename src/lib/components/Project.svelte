ce80966ea3036c7e0578bf4f0bb3e5ce99529a09
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-02-27 23:06:01 +1300

    Handle text better in project items

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -30,19 +30,19 @@
 					lg:opacity-0 transition-opacity duration-300 group-hover:opacity-100"></span>
 			</div>
 
-			<div class="text-blue-50 absolute inset-x-0 bottom-0 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+			<div class="text-blue-50 absolute inset-x-0 bottom-0 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-300 max-h-full overflow-y-auto">
 				<div
-					class="p-8 space-y-4 transform lg:translate-y-20 transition-transform duration-300 group-hover:translate-y-0">
-					<div class="space-y-2">
-						<h2 class="text-2xl font-bold">
+					class="p-4 md:p-6 lg:p-8 space-y-2 md:space-y-4 transform lg:translate-y-20 transition-transform duration-300 group-hover:translate-y-0">
+					<div class="space-y-1 md:space-y-2">
+						<h2 class="text-xl md:text-2xl font-bold">
 							{title}
 						</h2>
-						<p class="text-lg">
+						<p class="text-sm md:text-lg line-clamp-3 md:line-clamp-none">
 							{description}
 						</p>
 					</div>
 
-					<div class="flex flex-wrap gap-3">
+					<div class="sm:flex flex-wrap gap-2 md:gap-3 hidden">
 						{#each tags.slice(0, 10) as tag}
 							<Chip>
 								{tag}
