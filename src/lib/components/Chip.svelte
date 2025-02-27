09014bf0243e4eaefd6f50ff1fb5684907e3bf19
Brandon Scott-Hill <bran.scotthill@gmail.com>
2025-02-27 23:05:49 +1300

    Update chip to be smaller

--- src/lib/components/Chip.svelte
+++ src/lib/components/Chip.svelte
@@ -7,6 +7,6 @@
 	} = $props();
 </script>
 
-<div class="text-sm px-3 py-1 font-medium bg-zinc-900 dark:bg-blue-50 text-blue-50 dark:text-zinc-900 rounded-[50px] px-2">
+<div class="text-xs px-2 py-0.5 font-medium bg-zinc-900 dark:bg-blue-50 text-blue-50 dark:text-zinc-900 rounded-[50px] inline-flex items-center">
 	{@render children?.()}
 </div>
